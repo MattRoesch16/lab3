00919a18e805a2928c73cc49f8d7db1ff826f50b
MattRoesch16 <104652888+MattRoesch16@users.noreply.github.com>
2023-02-07 15:30:14 -0500

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @author Matthew Roesch
+ *
+ */
+
+public class Hello
+{
+	public static void main (String argv[])
+	{
+		System.out.println ("Hello, " + argv[0] + "!")
+	}
+}
